201ac51f49cc19b3401942e735bcf8d9882759ff
Howie Motenko <howie.motenko@jax.org>
2015-01-13 15:25:15 -0500

    refactor code for simpleList and simpleMap, add javadoc

--- src/org/jax/mgi/imsr/helpers/MGDConnection.java
+++ src/org/jax/mgi/imsr/helpers/MGDConnection.java
@@ -14,10 +14,17 @@ import org.jax.mgi.imsr.model.MgdAlleleMaps;
 import org.jax.mgi.imsr.model.MgiFeature;
 
 
+
 public class MGDConnection {
 
 	private static String url = "jdbc:postgresql://adhoc.informatics.jax.org/mgd";
 	
+    /**
+     * Runs a sql query on server (adhoc) and returns a result set.
+     * 
+     * @param query	- sql query string to run on server
+     * @return		the result set from running the query on the server
+     */
     public static ResultSet executeQuery(String query) {
     	ResultSet results = null;
     	
@@ -39,18 +46,23 @@ public class MGDConnection {
     	return results;
     }
     
-    public static HashMap<String, String> getRepoNomenclature(Integer logicalDB) {
+    
+	/**
+	 * Run a db sql query and populate a hash map with two columns. 
+	 * This is a direct population, without any logic.
+	 * 
+	 * @param query			- db sql query to run
+	 * @param colunmName1	- column name for the first tuple of the hash map
+	 * @param colunmName2	- column name for the second tuple of the hash map
+	 * @return				a hash map populated with results from the sql query
+	 */
+	public static HashMap<String, String> simpleHashMapQuery(String query, String colunmName1, String colunmName2) {
 		HashMap<String, String> resultHashMap = new HashMap<String, String>();
-    	String query = "SELECT a.accid, s.strain " +
-		               "FROM ACC_Accession a, PRB_Strain s " +
-    			       "WHERE a._logicaldb_key = " + logicalDB + " " +
-    			       "AND a._object_key = s._strain_key";
-    	
-    	ResultSet results = executeQuery(query);
-    	
+    	    	
     	try {
+    		ResultSet results = executeQuery(query);
 			while (results.next()) {
-				resultHashMap.put(results.getString("accid"), results.getString("strain"));
+				resultHashMap.put(results.getString(colunmName1), results.getString(colunmName2));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -59,18 +71,22 @@ public class MGDConnection {
     	return resultHashMap;
     }
 
-    public static List<String> getAllNomenclatures(List<Integer> logicalDBs) {
+
+	/**
+	 * Run a db sql query and populate a list with one column. 
+	 * This is a direct population, without any logic.
+	 * 
+	 * @param query			- db sql query to run
+	 * @param colunmName	- column name to populate the list with
+	 * @return				a list populated with results from the sql query
+	 */
+	public static List<String> simpleListQuery(String query, String colunmName) {
 		List<String> resultList = new ArrayList<String>();
-    	String query = "SELECT distinct s.strain " +
-		               "FROM ACC_Accession a, PRB_Strain s " +
-    			       "WHERE a._logicaldb_key IN (" + StringUtils.join(logicalDBs, ',') + ") " +
-    			       "AND a._object_key = s._strain_key";
-    	
-    	ResultSet results = executeQuery(query);
     	
     	try {
+    		ResultSet results = executeQuery(query);
 			while (results.next()) {	
-				resultList.add(results.getString("strain"));
+				resultList.add(results.getString(colunmName));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -79,16 +95,95 @@ public class MGDConnection {
     	return resultList;
     }
 
+
+    /**
+     * Returns a hash map of nomenclature for all strains from a specific repository.
+     * 
+     * @param logicalDB	- logical DB key of repository for nomenclature lookup
+     * @return			a hash map of <accession id, strain name> that lists all nomenclature within a logical DB 
+     */
+    public static HashMap<String, String> getRepoNomenclature(Integer logicalDB) {
+    	String query = "SELECT a.accid, s.strain " +
+		               "FROM ACC_Accession a, PRB_Strain s " +
+    			       "WHERE a._logicaldb_key = " + logicalDB + " " +
+    			       "AND a._object_key = s._strain_key";
+    	    	
+    	return simpleHashMapQuery(query, "accid", "strain");
+    }
+
+ 
+    /**
+     * Returns a hash map of all synonyms and the strain associated with the synonym.
+     * 
+     * @return	a hash map of <synonym, strain name> that lists all synonyms and the strain associated with the synonym
+     */
+    public static HashMap<String, String> getInverseSynonyms() {
+    	String query = "SELECT syn.synonym, str.strain " +
+		               "FROM MGI_Synonym syn, PRB_Strain str " +
+    			       "WHERE syn._mgitype_key = 10 " +
+    			       "AND syn._object_key = str._strain_key";
+    	
+    	return simpleHashMapQuery(query, "synonym", "strain");
+    }
+
+
+	/**
+	 * Returns a hash map of all withdrawn markers.
+	 * 
+	 * @return	a hash map of <symbol, name> that lists all withdrawn markers
+	 */
+	public static HashMap<String, String> getWithdrawnMarkers() {
+    	String query = "SELECT m.symbol, m.name " +
+		               "FROM MRK_Marker m " +
+    			       "WHERE m._marker_status_key = 2";
+    	
+    	return simpleHashMapQuery(query, "symbol", "name");
+    }
+
+
+	/**
+	 * Returns a list of strain names for all repositories provided.
+	 * 
+	 * @param logicalDBs	- list of repository logical DB's to gather strain names from
+	 * @return				- a lists all nomenclature within the specified logical DBs
+	 */
+	public static List<String> getAllNomenclatures(List<Integer> logicalDBs) {
+    	String query = "SELECT distinct s.strain " +
+		               "FROM ACC_Accession a, PRB_Strain s " +
+    			       "WHERE a._logicaldb_key IN (" + StringUtils.join(logicalDBs, ',') + ") " +
+    			       "AND a._object_key = s._strain_key";
+    	
+    	return simpleListQuery(query, "strain");
+    }
+
+	/**
+	 * Returns a list of accession ids of all recombinase alleles.
+	 * 
+	 * @return	a list of accession ids of all recombinase alleles
+	 */
+	public static List<String> getRecombinaseAlleles() {
+    	String query = "SELECT acc.accid " +
+    				   "FROM ACC_Accession acc, ALL_Allele a, VOC_Term t, VOC_Annot v " +
+    				   "WHERE acc._mgitype_key = 11 " +
+    				   "AND acc.private = 0 " +
+    				   "AND v._AnnotType_key = 1014 " +
+    				   "AND acc._object_key = a._allele_key " +
+    				   "AND v._object_key = a._allele_key " +
+    				   "AND  v._Term_key = t._Term_key " +
+    				   "AND t.term = 'Recombinase'";
+
+    	return simpleListQuery(query, "accid");
+	}
+
     public static HashMap<String, List<String>> getSynonyms() {
 		HashMap<String, List<String>> resultHashMap = new HashMap<String, List<String>> ();
     	String query = "SELECT syn.synonym, str.strain " +
 		               "FROM MGI_Synonym syn, PRB_Strain str " +
     			       "WHERE syn._mgitype_key = 10 " +
     			       "AND syn._object_key = str._strain_key";
-    	
-    	ResultSet results = executeQuery(query);
-    	
+    	  	
     	try {
+    		ResultSet results = executeQuery(query);
 			while (results.next()) {
 				String strain = results.getString("strain");
 				if (!resultHashMap.containsKey(strain)) {
@@ -96,26 +191,6 @@ public class MGDConnection {
 				}
 				List<String> synonyms = resultHashMap.get(strain);
 				synonyms.add(results.getString("synonym"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
-    	return resultHashMap;
-    }
-
-    public static HashMap<String, String> getInverseSynonyms() {
-		HashMap<String, String> resultHashMap = new HashMap<String, String> ();
-    	String query = "SELECT syn.synonym, str.strain " +
-		               "FROM MGI_Synonym syn, PRB_Strain str " +
-    			       "WHERE syn._mgitype_key = 10 " +
-    			       "AND syn._object_key = str._strain_key";
-    	
-    	ResultSet results = executeQuery(query);
-    	
-    	try {
-			while (results.next()) {
-				resultHashMap.put(results.getString("synonym"), results.getString("strain"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -133,9 +208,8 @@ public class MGDConnection {
     			       "AND acc._object_key = a._allele_key " +
     			       "AND acc.private = 0";
     	
-    	ResultSet results = executeQuery(query);
-    	
     	try {
+    		ResultSet results = executeQuery(query);
 			while (results.next()) {	
 				MgiFeature alleleDetail = new MgiFeature(results.getString("symbol"), results.getString("name"));
 				resultFeatureMap.put(results.getString("accid"), alleleDetail);
@@ -149,31 +223,11 @@ public class MGDConnection {
 		return mgdAlleleMaps;
 	}
 
-	public static List<String> getRecombinaseAlleles() {
-		List<String> resultList = new ArrayList<String>();
-    	String query = "SELECT acc.accid " +
-    				   "FROM ACC_Accession acc, ALL_Allele a, VOC_Term t, VOC_Annot v " +
-    				   "WHERE acc._mgitype_key = 11 " +
-    				   "AND acc.private = 0 " +
-    				   "AND v._AnnotType_key = 1014 " +
-    				   "AND acc._object_key = a._allele_key " +
-    				   "AND v._object_key = a._allele_key " +
-    				   "AND  v._Term_key = t._Term_key " +
-    				   "AND t.term = 'Recombinase'";
-
-    	ResultSet results = executeQuery(query);
-    	
-    	try {
-			while (results.next()) {	
-				resultList.add(results.getString("accid"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
-    	return resultList;
-	}
-
+	/**
+	 * Returns a hash map of all accession ids and their associated genes.
+	 * 
+	 * @return	a hash map of <accid, <gene symbol, gene name>> of all genes
+	 */
 	public static HashMap<String, MgiFeature> getGenes() {
 		HashMap<String, MgiFeature> resultFeatureMap = new HashMap<String, MgiFeature>();
     	String query = "SELECT acc.accid, m.symbol, m.name " +
@@ -181,10 +235,9 @@ public class MGDConnection {
     			       "WHERE acc._mgitype_key = 2 " +
     			       "AND acc._object_key = m._marker_key " +
     			       "AND acc.private = 0";
-    	
-    	ResultSet results = executeQuery(query);
-    	
+    	    	
     	try {
+        	ResultSet results = executeQuery(query);
 			while (results.next()) {
 				MgiFeature geneDetails = new MgiFeature(results.getString("symbol"), results.getString("name"));
 				resultFeatureMap.put(results.getString("accid"), geneDetails);
@@ -196,23 +249,5 @@ public class MGDConnection {
     	return resultFeatureMap;
 	}
 
-	public static HashMap<String, String> getWithdrawnMarkers() {
-		HashMap<String, String> resultHashMap = new HashMap<String, String>();
-    	String query = "SELECT m.symbol, m.name " +
-		               "FROM MRK_Marker m " +
-    			       "WHERE m._marker_status_key = 2";
-    	
-    	ResultSet results = executeQuery(query);
-    	
-    	try {
-			while (results.next()) {
-				resultHashMap.put(results.getString("symbol"), results.getString("name"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-    	
-    	return resultHashMap;
-    }
 
 }
