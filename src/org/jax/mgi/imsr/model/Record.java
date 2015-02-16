9f3f05e51583857edb50bd7267ba59a325a92841
Howie Motenko <howie.motenko@jax.org>
2015-02-16 16:06:50 -0500

    add javadoc and cleaned up some loose ends

--- src/org/jax/mgi/imsr/model/Record.java
+++ src/org/jax/mgi/imsr/model/Record.java
@@ -26,78 +26,77 @@ public class Record {
 	private String geneName;
 	private List<String> errors = new ArrayList<String>();
 	private List<String> warnings = new ArrayList<String>();
-	
-	
+
 	public Record() {
 		// must have no-argument constructor
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public Set<String> getTypes() {
 		return types;
 	}
-		
+
 	public Set<String> getStates() {
 		return states;
 	}
-	
+
 	public String getUrl() {
 		return url;
 	}
-	
+
 	public String getMgiAlleleAccId() {
 		return mgiAlleleAccId;
 	}
-	
+
 	public String getAlleleSymbol() {
 		return alleleSymbol;
 	}
-	
+
 	public String getAlleleName() {
 		return alleleName;
 	}
-	
+
 	public Set<String> getMutationTypes() {
 		return mutationTypes;
 	}
-	
+
 	public String getChromosome() {
 		return chromosome;
 	}
-	
+
 	public String getMgiGeneAccId() {
 		return mgiGeneAccId;
 	}
-	
+
 	public String getGeneSymbol() {
 		return geneSymbol;
 	}
-	
+
 	public String getGeneName() {
 		return geneName;
 	}
-	
+
 	public boolean isValid() {
 		return errors.isEmpty();
 	}
-	
+
 	public List<String> getErrors() {
 		return errors;
 	}
-	
+
 	public void addError(String error) {
 		if (error != null) {
 			errors.add(error);
 		}
 	}
-		
+
 	public boolean hasWarning() {
 		return !warnings.isEmpty();
 	}
@@ -105,13 +104,13 @@ public class Record {
 	public List<String> getWarnings() {
 		return warnings;
 	}
-	
+
 	public void addWarning(String warning) {
 		if (warning != null) {
 			warnings.add(warning);
 		}
 	}
-		
+
 	public void setId(String id) {
 		this.id = id;
 	}
@@ -148,7 +147,7 @@ public class Record {
 		if (mutationType != null && Constants.MUTATION_TYPES.containsKey(mutationType)) {
 			mutationTypes.add(mutationType);
 		} else {
-			System.out.println("ERROR: Mutation ype is invalid [" +  mutationType);
+			System.out.println("ERROR: Mutation type is invalid [" + mutationType + "]");
 		}
 	}
 
@@ -174,37 +173,43 @@ public class Record {
 
 	@Override
 	public String toString() {
-		return "Strain [id=" + id + ", name=" + name + ", types=" + types.toString()
-				+ ", states=" + states.toString() + ", url=" + url + ", mgiAlleleAccId="
-				+ mgiAlleleAccId + ", alleleSymbol=" + alleleSymbol
-				+ ", alleleName=" + alleleName + ", mutations="
-				+ mutationTypes.toString() + ", chromosome=" + chromosome
-				+ ", mgiGeneAccId=" + mgiGeneAccId + ", geneSymbol="
-				+ geneSymbol + ", geneName=" + geneName + ", isValid=" + isValid() + "]";
+		return "Strain [id=" + id + ", name=" + name + ", types="
+				+ types.toString() + ", states=" + states.toString() + ", url="
+				+ url + ", mgiAlleleAccId=" + mgiAlleleAccId
+				+ ", alleleSymbol=" + alleleSymbol + ", alleleName="
+				+ alleleName + ", mutations=" + mutationTypes.toString()
+				+ ", chromosome=" + chromosome + ", mgiGeneAccId="
+				+ mgiGeneAccId + ", geneSymbol=" + geneSymbol + ", geneName="
+				+ geneName + ", isValid=" + isValid() + "]";
 	}
-	
 
+	/**
+	 * Checks if gene accession id is a valid accession id.
+	 * Adds a warning to the record, if invalid.
+	 * 
+	 * @param geneMap	- map of valid gene accession id's
+	 */
 	public void validateMgiGeneAccId(HashMap<String, MgiFeature> geneMap) {
 		if (this.mgiGeneAccId != null && !this.mgiGeneAccId.isEmpty()) {
 			if (!geneMap.containsKey(this.mgiGeneAccId)) {
-				if (this.geneSymbol == null || this.geneSymbol.isEmpty()) {
-					this.addWarning("Strain Gene Accession ID is invalid: [" + this.mgiGeneAccId + "]");
-				}
+				this.addWarning("Strain Gene Accession ID is invalid: [" + this.mgiGeneAccId + "]");
 			}
 		}
 	}
 
-
-	public void validateMgiAlleleAccId(HashMap<String,MgiFeature> alleleMap) {
+	/**
+	 * Checks if allele accession id is a valid accession id.
+	 * Adds a warning to the record, if invalid.
+	 * 
+	 * @param alleleMap	- map of valid allele accession id's
+	 */
+	public void validateMgiAlleleAccId(HashMap<String, MgiFeature> alleleMap) {
 		if (this.mgiAlleleAccId != null && !this.mgiAlleleAccId.isEmpty()) {
 			if (!alleleMap.containsKey(mgiAlleleAccId)) {
-				if (this.alleleSymbol == null || this.alleleSymbol.isEmpty()) {
-					this.addWarning("Strain Allele Accession ID is invalid: [" + mgiAlleleAccId + "]");
-				}
+				this.addWarning("Strain Allele Accession ID is invalid: [" + mgiAlleleAccId + "]");
 			}
 		}
 	}
-
 
 	public void validateId() {
 		if (this.id == null || this.id.isEmpty()) {
@@ -212,35 +217,31 @@ public class Record {
 		}
 	}
 
-	
 	public void validateUrl() {
 		if (this.url == null || this.url.isEmpty()) {
 			this.addError("Strain URL: is a required field");
-		}		
+		}
 	}
 
-	
 	public void validateStates() {
 		if (this.states == null || this.states.isEmpty()) {
 			this.addError("Strain State: is a required field");
 		}
-		
+
 		if (Constants.STRAIN_STATES.keySet().containsAll(getStates()) == false) {
 			this.addError("Strain State: contains an invalid state: [" + getStates().toString() + "]");
-		}	
+		}
 	}
-
 
 	public void validateTypes() {
 		if (this.types == null || this.types.isEmpty()) {
 			this.addError("Strain Type: is a required field");
 		}
-		
+
 		if (Constants.STRAIN_TYPES.keySet().containsAll(getTypes()) == false) {
 			this.addError("Strain Type: contains an invalid type: [" + getTypes().toString() + "]");
-		}	
+		}
 	}
-
 
 	public void validateName() {
 		if (this.id == null || this.id.isEmpty()) {
@@ -248,83 +249,98 @@ public class Record {
 		}
 	}
 
-	
 	public void validateMutationType() {
 		if (this.mutationTypes != null && !this.mutationTypes.isEmpty()) {
 			if (Constants.MUTATION_TYPES.keySet().containsAll(getMutationTypes()) == false) {
 				this.addWarning("Strain Mutation Type: contains an invalid mutation type: [" + getMutationTypes().toString() + "]");
-			}	
+			}
 		}
 	}
 
-
-	public void transformAllele(MgdAlleleMaps alleleFeaturesMap, List<String> recombinaseAlleleList) {		
+	/**
+	 * Using allele accession id, replace symbol and name with mgi data and add recombinase mutation type (if needed).
+	 * If invalid allele acc id, use allele symbol to find acc id.
+	 * 
+	 * @param alleleFeaturesMap		- map of valid allele accession id's
+	 * @param recombinaseAlleleList	- mgi known allele recombinase list 
+	 */
+	public void transformAllele(MgdAlleleMaps alleleFeaturesMap, List<String> recombinaseAlleleList) {
 		HashMap<String, MgiFeature> alleleMap = alleleFeaturesMap.getFeatureMap();
 		HashMap<String, String> alleleSymbolMap = alleleFeaturesMap.getsymbolMap();
 
-		if (mgiAlleleAccId != null && !mgiAlleleAccId.isEmpty() && alleleMap.containsKey(mgiAlleleAccId)) {			
+		if (mgiAlleleAccId != null && !mgiAlleleAccId.isEmpty() && alleleMap.containsKey(mgiAlleleAccId)) {
 			// ignore repository information and use mgi data
 		} else if (alleleSymbol != null && !alleleSymbol.isEmpty() && alleleSymbolMap.containsKey(alleleSymbol)) {
 			// find accession id, to create a valid link
-			mgiAlleleAccId = alleleSymbolMap.get(alleleSymbol);			
+			mgiAlleleAccId = alleleSymbolMap.get(alleleSymbol);
 		} else {
 			// prevent link to invalid allele accid
 			mgiAlleleAccId = null;
 		}
-		
+
 		if (mgiAlleleAccId != null) {
 			// ignore repository information and use mgi data
 			alleleSymbol = alleleMap.get(mgiAlleleAccId).getSymbol();
 			alleleName = alleleMap.get(mgiAlleleAccId).getName();
-			
+
 			if (recombinaseAlleleList.contains(mgiAlleleAccId)) {
 				addMutationType("REC");
 			}
 		}
 	}
 
-
+	/**
+	 * Using gene accession id, replace symbol and name with mgi data and add withdrawn information (if needed).
+	 * 
+	 * @param geneMap				- map of valid gene accession id's
+	 * @param withdrawnMarkersMap	- mgi list of withdrawn gene accession id's 
+	 */
 	public void transformGene(HashMap<String, MgiFeature> geneMap, HashMap<String, String> withdrawnMarkersMap) {
 		if (mgiGeneAccId != null && !mgiGeneAccId.isEmpty() && geneMap.containsKey(mgiGeneAccId)) {
-			// ignore repository information and use mgi data
+			// replace repository information with  mgi data
 			geneSymbol = geneMap.get(mgiGeneAccId).getSymbol();
-			geneName = geneMap.get(mgiGeneAccId).getName();						
+			geneName = geneMap.get(mgiGeneAccId).getName();
 		} else {
-			//prevent link to invalid gene accid
+			// prevent link to invalid gene accid
 			mgiGeneAccId = null;
-			
-			//add withdrawn information
+
+			// add withdrawn information
 			if (geneSymbol != null && !geneSymbol.isEmpty() && withdrawnMarkersMap.containsKey(geneSymbol)) {
 				geneName = withdrawnMarkersMap.get(geneSymbol);
 			}
-		}		
+		}
 	}
 
-
+	/**
+	 * Insert id in strain url
+	 * 
+	 * @param strainUrl	- url string for insertion of id
+	 */
 	public void transformUrl(String strainUrl) {
 		if (strainUrl != null && !strainUrl.isEmpty()) {
 			url = strainUrl.replace("###STRAINURL###", id);
 		}
 	}
 
-	
+	/**
+	 * Check repsonse status code of url. If code is a warning or error, add warning or error
+	 * to respective lists.
+	 */
 	public void testUrlConnection() {
 		int code;
-		
+
 		try {
 			code = URLHelper.getHTTPResponseStatusCode(this.url);
-			
+
 			if (code >= 300 && code < 400) {
 				this.addWarning("Strain URL: [" + this.url + "]" + " with a redirect response code = " + code);
-			}
-			else if (code != 200) {
+			} else if (code != 200) {
 				this.addError("Strain URL: [" + this.url + "]" + " with response code = " + code);
 			}
-			
+
 		} catch (IOException e) {
 			this.addError("EXCEPTION - Strain: " + this.id + " - URL: [" + this.url + "]" + " reports " + e.getMessage());
 		}
 	}
 
 }
-
