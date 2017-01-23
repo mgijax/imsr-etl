08ab7bf4496e2c4becd89e038dbf8113d4f621e7
Howie Motenko <howie.motenko@jax.org>
2017-01-23 09:29:19 -0500

    add unit tests for Utilities and createRecord

--- test/org/jax/mgi/imsr/helpers/UtilitiesTest.java
+++ test/org/jax/mgi/imsr/helpers/UtilitiesTest.java
@@ -14,54 +14,6 @@ import org.mockito.Mockito;
 public class UtilitiesTest {
 
 	//
-	// pluralSuffix() Tests
-	//
-	
-	@Test
-	public void oneCountReturnsSingular() {
-		Integer exactlyOneCount = 1;
-		String singularString = "test";
-		String pluralString = "tests";
-		
-		String result = Utilities.pluralSuffix(exactlyOneCount, singularString, pluralString);
-		
-		assertThat(result, is(equalTo(singularString)));
-	}
-	
-	
-	@Test
-	public void greaterThanOneCountReturnsPlural() {
-		Integer greaterThanOneCount = 5;
-		String singularString = "test";
-		String pluralString = "tests";
-		
-		String result = Utilities.pluralSuffix(greaterThanOneCount, singularString, pluralString);
-		
-		assertThat(result, is(equalTo(pluralString)));
-	}
-	
-	@Test
-	public void lessThanOneCountReturnsPluralString() {
-		Integer lessThanOneCount = 0;
-		String singularString = "test";
-		String pluralString = "tests";
-		
-		String result = Utilities.pluralSuffix(lessThanOneCount, singularString, pluralString);
-		
-		assertThat(result, is(equalTo(pluralString)));
-	}
-	
-	@Test(expected=NullPointerException.class)
-	public void throwsExceptionWhenCountIsNull() {
-		Integer nullCount = null;
-		String singularString = "test";
-		String pluralString = "tests";
-		
-		Utilities.pluralSuffix(nullCount, singularString, pluralString);
-	}
-
-	
-	//
 	// newerFile() Tests
 	//
 	
@@ -70,9 +22,9 @@ public class UtilitiesTest {
 		File olderFile = null;
 		File newFile = null;
 		
-		File result = Utilities.newerFile(newFile, olderFile);
+		File actualResult = Utilities.newerFile(newFile, olderFile);
 		
-		assertThat(result, is(equalTo(null)));
+		assertThat(actualResult, is(equalTo(null)));
 	}
 
 	@Test
@@ -80,9 +32,9 @@ public class UtilitiesTest {
 		File olderFile = Mockito.mock(File.class);
 		File newFile = null;
 		
-		File result = Utilities.newerFile(newFile, olderFile);
+		File actualResult = Utilities.newerFile(newFile, olderFile);
 		
-		assertThat(result, is(equalTo(olderFile)));
+		assertThat(actualResult, is(equalTo(olderFile)));
 	}
 
 	@Rule
@@ -98,11 +50,98 @@ public class UtilitiesTest {
 		oldFile.setLastModified(olderTime);
 		newFile.setLastModified(newerTime);
 		
-		File result = Utilities.newerFile(newFile, oldFile);
+		File actualResult = Utilities.newerFile(newFile, oldFile);
 		
-		assertThat(result, is(equalTo(newFile)));
+		assertThat(actualResult, is(equalTo(newFile)));
 	}
 
+	
+	//
+	// pluralSuffix() Tests
+	//
+	
+	@Test
+	public void oneCountReturnsSingular() {
+		Integer exactlyOneCount = 1;
+		String singularString = "test";
+		String pluralString = "tests";
+		
+		String actualResult = Utilities.pluralSuffix(exactlyOneCount, singularString, pluralString);
+		
+		assertThat(actualResult, is(equalTo(singularString)));
+	}
+	
+	
+	@Test
+	public void greaterThanOneCountReturnsPlural() {
+		Integer greaterThanOneCount = 5;
+		String singularString = "test";
+		String pluralString = "tests";
+		
+		String actualResult = Utilities.pluralSuffix(greaterThanOneCount, singularString, pluralString);
+		
+		assertThat(actualResult, is(equalTo(pluralString)));
+	}
+	
+	@Test
+	public void lessThanOneCountReturnsPluralString() {
+		Integer lessThanOneCount = 0;
+		String singularString = "test";
+		String pluralString = "tests";
+		
+		String actualResult = Utilities.pluralSuffix(lessThanOneCount, singularString, pluralString);
+		
+		assertThat(actualResult, is(equalTo(pluralString)));
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void throwsExceptionWhenCountIsNull() {
+		Integer nullCount = null;
+		String singularString = "test";
+		String pluralString = "tests";
+		
+		Utilities.pluralSuffix(nullCount, singularString, pluralString);
+	}
+
+	
+	//
+	// stripDoubleQuotes() Tests
+	//
+	
+	@Test
+	public void noQuotesToRemoveFromString() {
+		String noDoubleQuotes = "This is a \t valid STRING of characters!";
+		
+		String actualResult = Utilities.stripDoubleQuotes(noDoubleQuotes);
+		
+		assertThat(actualResult, is(equalTo(noDoubleQuotes)));
+	}
+	
+	@Test
+	public void quotesToBeRemovedFromString() {
+		String someDoubleQuotes = "A string - \" \" - with double quotes in the middle.";
+		String noDoubleQuotes = "A string -   - with double quotes in the middle.";
+		
+		String actualResult = Utilities.stripDoubleQuotes(someDoubleQuotes);
+		
+		assertThat(actualResult, is(equalTo(noDoubleQuotes)));
+	}
+	
+	@Test
+	public void emptyStringReturnsEmptyString_DoubleQuoteTest() {
+		String emptyString = "";
+		
+		String actualResult = Utilities.stripDoubleQuotes(emptyString);
+		
+		assertThat(actualResult, is(equalTo(emptyString)));
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void nullStringReturnsNullString_DoubleQuoteTest() {
+		String nullString = null;
+		
+		Utilities.stripDoubleQuotes(nullString);
+	}
 	
 	//
 	// stripNonAsciiChars() Tests
@@ -112,9 +151,9 @@ public class UtilitiesTest {
 	public void noCharsRemovedFromValidString() {
 		String allValidChars = "This is a \t valid STRING of characters!";
 		
-		String result = Utilities.stripNonAsciiChars(allValidChars);
+		String actualResult = Utilities.stripNonAsciiChars(allValidChars);
 		
-		assertThat(result, is(equalTo(allValidChars)));
+		assertThat(actualResult, is(equalTo(allValidChars)));
 	}
 	
 	@Test
@@ -122,18 +161,18 @@ public class UtilitiesTest {
 		String someNonValidChars = "Here are valid chars - ¡öÖÊ¿© - with non-valid in the middle.";
 		String onlyValidChars = "Here are valid chars -  - with non-valid in the middle.";
 		
-		String result = Utilities.stripNonAsciiChars(someNonValidChars);
+		String actualResult = Utilities.stripNonAsciiChars(someNonValidChars);
 		
-		assertThat(result, is(equalTo(onlyValidChars)));
+		assertThat(actualResult, is(equalTo(onlyValidChars)));
 	}
 	
 	@Test
 	public void emptyStringReturnsEmptyString() {
 		String emptyString = "";
 		
-		String result = Utilities.stripNonAsciiChars(emptyString);
+		String actualResult = Utilities.stripNonAsciiChars(emptyString);
 		
-		assertThat(result, is(equalTo(emptyString)));
+		assertThat(actualResult, is(equalTo(emptyString)));
 	}
 	
 	@Test(expected=NullPointerException.class)
